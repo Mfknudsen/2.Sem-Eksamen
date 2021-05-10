154e25c76f28d6bd54528f2d505722a3b6280984
Mads Knudsen <madsfknudsen@hotmail.com>
2021-05-10 14:03:27 +0200

    Starting styklist hashmap

--- src/main/java/business/persistence/Planner.java
+++ src/main/java/business/persistence/Planner.java
@@ -1,16 +1,24 @@
 package business.persistence;
 
 import business.entities.Styklist;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class Planner {
-    public Styklist Calculate(float length, float width){
+    //Length and Width in meters
+    public Styklist Calculate(float length, float width, boolean shed, float sLength, float sWidth){
         Styklist result = new Styklist();
         //https://www.johannesfog.dk/byggecenter/produkter/2CARPORT_CAR01/
         //First version based on average price of m2 of referred product.
         float avgPrice = 344.9f;
         float m2 = length * width;
 
-        result.price = m2 * avgPrice;
+        result.setPrice(m2 * avgPrice);
+
+        if(shed){
+
+        }
 
         return result;
     }
